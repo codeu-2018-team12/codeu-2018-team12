a24b4a3a32cd890ec875a3ddd9fff482f8d83b5a
Maria Mahin <mariamcm90@gmail.com>
2018-03-04 13:37:54 -0500

    Modified RegisterServlet.java to detect whether a username is taken and register new users

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -1,6 +1,10 @@
 package codeu.controller;
 
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
 import java.io.IOException;
+import java.util.UUID;
+import java.time.Instant;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +12,29 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Servlet class responsible for user registration. */
 public class RegisterServlet extends HttpServlet {
+
+  /**
+   * Store class that gives access to Users.
+   */
+  private UserStore userStore;
+
+  /**
+   * Set up state for handling registration-related requests. This method is only called when
+   * running in a server, not when running in a test.
+   */
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    setUserStore(UserStore.getInstance());
+  }
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,7 +56,15 @@ public class RegisterServlet extends HttpServlet {
       return;
     }
 
-    response.getWriter().println("<p>Username: " + username + "</p>");
-    response.getWriter().println("<p>Password: " + password + "</p>");
+    if (userStore.isUserRegistered(username)) {
+      request.setAttribute("error", "That username is already taken.");
+      request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+      return;
+    }
+
+    User user = new User(UUID.randomUUID(), username, password, Instant.now());
+    userStore.addUser(user);
+
+    response.sendRedirect("/login");
   }
 }
