9e0010e7158215ce9693ddea5a823911d7c8b552
Maria Mahin <mariamcm90@gmail.com>
2018-03-21 12:34:58 -0400

    Added a getMostRecentMessageFromConvo() method to retrieve the most recent message from a given conversation

--- src/main/java/codeu/model/store/basic/MessageStore.java
+++ src/main/java/codeu/model/store/basic/MessageStore.java
@@ -102,6 +102,20 @@ public class MessageStore {
     return messages;
   }
 
+  /** Access the content of the most recent message within a given conversation */
+  public String getMostRecentMessageFromConvo(UUID conversationId){
+
+    String recentMessage = "";
+    messages.sort(msgComparator);
+
+    for (Message message: messages) {
+      if (message.getConversationId().equals(conversationId))
+        recentMessage = message.getContent();
+        break;
+    }
+    return recentMessage;
+  }
+
   /** Access the current set of Messages within the given Conversation. */
   public List<Message> getMessagesInConversation(UUID conversationId) {
 
