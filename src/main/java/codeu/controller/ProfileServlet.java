0b571b04cdfc79aa6e6342737b20f10601178d05
Maria Mahin <mariamcm90@gmail.com>
2018-03-26 20:32:18 -0400

    CircleCI changes

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /** Servlet class responsible for the profile page. */
 public class ProfileServlet extends HttpServlet {
 
