c11d1469a8841d22bf1e264f569cf9cdc46debd2
mariezimmy <mariezimmy@users.noreply.github.com>
2018-03-27 19:35:08 -0400

    okay, I *think* I appeased the circleCi stuff!

--- src/test/java/codeu/controller/ChatServletTest.java
+++ src/test/java/codeu/controller/ChatServletTest.java
@@ -316,7 +316,8 @@ public class ChatServletTest {
     Mockito.when(mockRequest.getRequestURI()).thenReturn("/chat/test_conversation");
     Mockito.when(mockSession.getAttribute("user")).thenReturn("test_username");
 
-    User fakeUser = new User(UUID.randomUUID(), "test_username", "password","testbiography", Instant.now());
+    User fakeUser =
+        new User(UUID.randomUUID(), "test_username", "password", "testbiography", Instant.now());
     Mockito.when(mockUserStore.getUser("test_username")).thenReturn(fakeUser);
 
     Conversation fakeConversation =
