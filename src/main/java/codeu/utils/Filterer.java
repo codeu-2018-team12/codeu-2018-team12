9bb7135b1673f12a42bf09f9d7540377d38ef34a
kkane098 <kkane098@gmail.com>
2018-04-20 14:10:04 -0400

    Filled in logic for filtering conversations based on the search string

--- src/main/java/codeu/utils/Filterer.java
+++ src/main/java/codeu/utils/Filterer.java
@@ -1,13 +1,102 @@
+package codeu.utils;
+
 import codeu.model.data.Conversation;
 import codeu.model.data.Message;
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 public class Filterer {
 
-  public static List<Conversation> filterConversationsByTitle(
+  public static List<Conversation> filterConversations(List<Conversation> convos, String input) {
+    String[] tokens = input.split("((?<=\\())|((?=\\)))| ");
+    List<String> tokensList = new ArrayList<String>(Arrays.asList(tokens));
+    return filterConversationsByTokens(convos, convos, tokensList);
+  }
+
+  private static List<Conversation> filterConversationsByTokens(
+      List<Conversation> originalConvos, List<Conversation> convos, List<String> tokens) {
+    if (tokens.size() == 0) {
+      return convos;
+    }
+    List<Conversation> filteredConvos =
+        filterConversationsByTokensHelper(originalConvos, convos, tokens);
+    if (tokens.size() == 0) {
+      return filteredConvos;
+    }
+    if (tokens.get(0).equals("AND")) {
+      tokens.remove(0);
+      return filterConversationsByTokens(originalConvos, filteredConvos, tokens);
+    } else if (tokens.get(0).equals("OR")) {
+      tokens.remove(0);
+      filteredConvos.addAll(filterConversationsByTokens(originalConvos, originalConvos, tokens));
+      return filteredConvos;
+    } else {
+      return filteredConvos;
+    }
+  }
+
+  private static List<Conversation> filterConversationsByTokensHelper(
+      List<Conversation> originalConvos, List<Conversation> convos, List<String> tokens) {
+    List<Conversation> filteredConvos = convos;
+    DateTimeFormatter formatter =
+        DateTimeFormatter.ofPattern("MM-dd-yyyy").withZone(ZoneId.systemDefault());
+    if (tokens.get(0).equals("(")) {
+      tokens.remove(0);
+      filteredConvos = filterConversationsByTokens(originalConvos, convos, tokens);
+      if (tokens.size() == 0 || !tokens.get(0).equals(")")) {
+        throw new UnsupportedOperationException(
+            "Incorrect string format - mismatched parentheses.");
+      } else {
+        tokens.remove(0);
+        return filteredConvos;
+      }
+    } else if (tokens.get(0).startsWith("before:")) {
+      String dateString = tokens.get(0).substring("before:".length());
+      tokens.remove(0);
+      LocalDate date = LocalDate.parse(dateString, formatter);
+      Instant instant = date.atStartOfDay(ZoneId.systemDefault()).toInstant();
+      filteredConvos = filterConversationsByCreationDate(convos, instant, -1);
+      return filteredConvos;
+    } else if (tokens.get(0).startsWith("after:")) {
+      String dateString = tokens.get(0).substring("after:".length());
+      tokens.remove(0);
+      LocalDate date = LocalDate.parse(dateString, formatter);
+      Instant instant = date.atStartOfDay(ZoneId.systemDefault()).toInstant();
+      filteredConvos = filterConversationsByCreationDate(convos, instant, 1);
+      return filteredConvos;
+    } else if (tokens.get(0).startsWith("on:")) {
+      String dateString = tokens.get(0).substring("on:".length());
+      tokens.remove(0);
+      LocalDate date = LocalDate.parse(dateString, formatter);
+      Instant instant = date.atStartOfDay(ZoneId.systemDefault()).toInstant();
+      filteredConvos = filterConversationsByCreationDate(convos, instant, 0);
+      return filteredConvos;
+    } else if (tokens.get(0).startsWith("with:")) {
+      String username = tokens.get(0).substring("with:".length());
+      System.out.println(username);
+      tokens.remove(0);
+      User user = UserStore.getInstance().getUser(username);
+      if (user == null) {
+        return new ArrayList<Conversation>();
+      }
+      filteredConvos = filterConversationsByMember(convos, user.getId());
+      return filteredConvos;
+    } else {
+      filteredConvos = filterConversationsByTitle(convos, tokens.get(0));
+      tokens.remove(0);
+      return filteredConvos;
+    }
+  }
+
+  private static List<Conversation> filterConversationsByTitle(
       List<Conversation> convos, String title) {
     ArrayList<Conversation> filteredConvos = new ArrayList<Conversation>();
     for (Conversation convo : convos) {
@@ -18,7 +107,7 @@ public class Filterer {
     return filteredConvos;
   }
 
-  public static List<Conversation> filterConversationsByCreationDate(
+  private static List<Conversation> filterConversationsByCreationDate(
       List<Conversation> convos, Instant date, int comp) {
     ArrayList<Conversation> filteredConvos = new ArrayList<Conversation>();
     for (Conversation convo : convos) {
@@ -33,7 +122,7 @@ public class Filterer {
     return filteredConvos;
   }
 
-  public static List<Conversation> filterConversationsByMember(
+  private static List<Conversation> filterConversationsByMember(
       List<Conversation> convos, UUID user) {
     ArrayList<Conversation> filteredConvos = new ArrayList<Conversation>();
     for (Conversation convo : convos) {
@@ -44,7 +133,7 @@ public class Filterer {
     return filteredConvos;
   }
 
-  public static List<Message> filterMessagesByContent(List<Message> messages, String content) {
+  private static List<Message> filterMessagesByContent(List<Message> messages, String content) {
     ArrayList<Message> filteredMessages = new ArrayList<Message>();
     for (Message message : messages) {
       if (message.getContent().contains(content)) {
@@ -54,7 +143,7 @@ public class Filterer {
     return filteredMessages;
   }
 
-  public static List<Message> filterMessagesByAuthor(List<Message> messages, UUID user) {
+  private static List<Message> filterMessagesByAuthor(List<Message> messages, UUID user) {
     ArrayList<Message> filteredMessages = new ArrayList<Message>();
     for (Message message : messages) {
       if (message.getAuthorId().equals(user)) {
@@ -64,7 +153,7 @@ public class Filterer {
     return filteredMessages;
   }
 
-  public static List<Message> filterMessagesByCreationDate(
+  private static List<Message> filterMessagesByCreationDate(
       List<Message> messages, Instant date, int comp) {
     ArrayList<Message> filteredMessages = new ArrayList<Message>();
     for (Message message : messages) {
